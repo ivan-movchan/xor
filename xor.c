2034afdc4f78dbda81a7699c72a4d0307bea7f18
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-03-27 17:28:52 +0300

    Little code optimizations
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- xor.c
+++ xor.c
@@ -4,37 +4,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define VERSION  "1.1"
+#define VERSION  "1.2"
 #define YEARS    "2025"
 #define AUTHOR   "Ivan Movchan <ivan.movchan.07@gmail.com>"
 #define HOMEPAGE "https://github.com/ivan-movchan/xor"
 
+void usage()
+{
+	puts("Usage: xor [-v] value [< input_file [> output_file]]");
+	exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char** argv)
 {
-    if (argc == 1)
-    {
-        puts("Usage: xor [-v] value [< input_file [> output_file]]");
-        return EXIT_SUCCESS;
-    };
+	int value = 0;
 
-    if (!strcmp(argv[1], "-v"))
-    {
-        printf("xor %s\nCopyright (C) %s %s\n%s\n", VERSION, YEARS, AUTHOR, HOMEPAGE);
-        return EXIT_SUCCESS;
-    };
+	for (int i = 1; i < argc; ++i)
+	{
+		if (!strcmp(argv[i], "-v"))
+		{
+			printf("xor %s\nCopyright (C) %s %s\n%s\n", VERSION, YEARS, AUTHOR, HOMEPAGE);
+			return EXIT_SUCCESS;
+		}
+		else
+		{
+			value = atoi(argv[i]);
 
-    int value = atoi(argv[1]);
+			if ((value == 0) && strcmp(argv[i], "0"))
+			{
+				usage();
+			};
+		};
+	};
 
-    if (value == 0)
-    {
-        puts("atoi returned 0 - nothing interesting will happen. Aborting.");
-        return EXIT_SUCCESS;
-    };
+	if (value == 0)
+	{
+		puts("atoi returned 0 - nothing interesting will happen. Aborting.");
+		return EXIT_SUCCESS;
+	};
 
-    while (!feof(stdin))
-    {
-        putchar((fgetc(stdin) ^ value));
-    };
+	while (!feof(stdin))
+	{
+		putchar((fgetc(stdin) ^ value));
+	};
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
